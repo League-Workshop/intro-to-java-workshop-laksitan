da08b9a2f8e92e0e3dffc3cf1beb18a4b4d05c77
root <root@2a8b9bd05ea2>
2020-04-15 02:59:34 +0000

    working on fourSquare

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,18 +6,20 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+    Robot sam = new Robot();
+    		
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+         sam.setSpeed(100);
 		// 5. Set the pen width to 5
-
+         sam.pendown
+         sam.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+                drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
@@ -26,8 +28,14 @@ public class FourSquare {
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		      sam.move(100);
+		      sam.turn(90);
+		      sam.move(100);
+		      sam.turn(90);
+		      sam.move(100);
+		      sam.turn(90);
+		      sam.move(100);
+		      
 	}
 
 	public static void main(String[] args) {
