f387072a47afaf95c3f97fc5e8d96653f6d514b9
root <root@94240d47e9a5>
2020-05-13 03:00:17 +0000

    AwesomeorNot

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,32 +2,50 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+	public static void main(String[] args) {
 
-	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+		// 2. Make a variable that will hold a random number and put a random number
+		// into this variable using "new Random().nextInt(4);
 
-	// 3. Print out this variable
+		Random r = new Random();
+		int number = r.nextInt(4); // 0 ---> 3
 
-	// 4. Get the user to enter something that they think is awesome
+		// 3. Print out this variable
+		System.out.println(number);
+		// 4. Get the user to enter something that they think is awesome
+		JOptionPane.showMessageDialog(null, "Enter something that you think is awesome");
+		// 5. If the random number is 0
 
-	// 5. If the random number is 0
+		if (number == 0) {
+			// -- tell the user whatever they entered is awesome!
+			JOptionPane.showMessageDialog(null, "That is awesome");
 
-	// -- tell the user whatever they entered is awesome!
+		}
 
-	// 6. If the random number is 1
+		// 6. If the random number is 1
+		if (number == 1) {
+			// -- tell the user whatever they entered is ok.
+			JOptionPane.showMessageDialog(null, "This is ok");
 
-	// -- tell the user whatever they entered is ok.
+		}
 
-	// 7. If the random number is 2
+		// 7. If the random number is 2
+		if (number == 2) {
+			// -- tell the user whatever they entered is boring.
+			JOptionPane.showMessageDialog(null, "This is boring");
+		}
 
-	// -- tell the user whatever they entered is boring.
-
-	// 8. If the random number is 3
-
-	// -- write your own answer
+		// 8. If the random number is 3
+		if (number == 3) {
+			JOptionPane.showMessageDialog(null, "This is extraordinary");
+			// -- tell user whatever the entered is extraordinary.
+		}
+	}
 
 }
-
 
